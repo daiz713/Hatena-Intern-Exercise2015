e9f59f4d6b78ac66d4cd2061ecf059ae348417a9
daiki <dev.da.iz.0x@gmail.com>
2015-07-17 01:41:12 +0900

    課題JS-2を解きました

--- js/main.js
+++ js/main.js
@@ -27,3 +27,37 @@ function parseLTSVLog(logStr) {
 }
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+/* tagName 要素を表す DOM オブジェクトを、parentObjの直下に追加して、返す関数 */
+function elem(tagName, text, parentObj) {
+  var newObj = document.createElement(tagName);
+  if(text != '') newObj.appendChild(document.createTextNode(text));
+  parentObj.appendChild(newObj);
+  return newObj;
+}
+
+/*
+ * 第1引数: div 要素を表す DOM オブジェクト
+ * 第2引数: 関数 parseLTSVLog の返り値と同じ形式の配列
+ * 第1引数のdivの直下にtableを生成する関数
+ */
+function createLogTable(containerDiv, logObjs) {
+  var labels = Object.keys(logObjs[0] || {});
+  var i, j;
+
+  var table = elem('table', '', containerDiv);
+  var thead = elem('thead', '', table);
+  var tbody = elem('tbody', '', table);
+  var tr, th, td, logObj;
+  
+  // <thead> ... </thead> の部分を生成:
+  tr = elem('tr', '', thead);
+  for(i = 0; i < labels.length; i++) elem('th', labels[i], tr);
+
+  // <tbody> ... </tbody> の部分を生成:
+  for(i = 0; i < logObjs.length; i++) {
+    logObj = logObjs[i];
+    tr = elem('tr', '', tbody);
+    for(j = 0; j < labels.length; j++) elem('td', logObj[labels[j]], tr);
+  }
+}
+
