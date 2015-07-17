65fca64dd9af5a3e9e7203e429fd2dfc5a120907
daiki <dev.da.iz.0x@gmail.com>
2015-07-17 11:31:24 +0900

    課題JS-3を解きました

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,9 @@
 // 課題 JS-3 の実装をここに記述してください。
+
+// ボタンにクリックイベントを登録:
+document.querySelector('#submit-button').addEventListener('click', function(e) {
+  var container = document.querySelector('#table-container');
+  var logStr = document.querySelector('#log-input').value;
+  container.innerHTML = '';  // 前回の描画をクリアする
+  createLogTable(container, parseLTSVLog(logStr));
+}, false);
