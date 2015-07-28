270de178b81e16c8b88f9e660a4af79cb7499737
daiki <dev.da.iz.0x@gmail.com>
2015-07-28 23:32:49 +0900

    Perl-1 を解きました

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -1,6 +1,8 @@
 package Log;
 use strict;
 use warnings;
+use DateTime;
+use DateTime::TimeZone;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,18 +10,40 @@ sub new {
 }
 
 sub protocol {
+  my $self = shift;
+  my @req = split(/ /, $self->{req});  # ["GET", "/apache_pb.gif", "HTTP/1.0"]
+  my $protocol = $req[2];
+  return $protocol;
 }
 
 sub method {
+  my $self = shift;
+  my @req = split(/ /, $self->{req});
+  my $method = $req[0];
+  return $method;
 }
 
 sub path {
+  my $self = shift;
+  my @req = split(/ /, $self->{req});
+  my $path = $req[1];
+  return $path;
 }
 
 sub uri {
+  my $self = shift;
+  my $host = $self->{host};
+  my $path =  $self->path;
+  return "http://" . $host . $path;
 }
 
 sub time {
+  my $self = shift;
+  my $time = DateTime->from_epoch(
+    epoch     => ($self->{epoch}),
+    time_zone => 'GMT'
+  );
+  return $time;
 }
 
 1;
