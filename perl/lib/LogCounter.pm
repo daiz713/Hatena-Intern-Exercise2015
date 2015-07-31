506a5087c5f07b84c09b31e4717d654de8de910f
daiki <dev.da.iz.0x@gmail.com>
2015-07-31 23:31:32 +0900

    Perl-3 を解きました

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -8,9 +8,40 @@ sub new {
 };
 
 sub group_by_user {
+  my $self = shift;
+  my @logs = @{$self->{logs}};
+  # 返り値用のリファレンス
+  my $res = {};
+
+  for my $log (@logs) {
+    # ユーザー名または"guest"を得る
+    my $user_name = (exists $log->{user}) ? $log->{user} : "guest";
+    # まだユーザー名のキーが無ければ作る
+    if(not exists $res->{$user_name}) {
+      $res->{$user_name} = [];
+    }
+    # ユーザー名の配列に追加する
+    push $res->{$user_name}, $log;
+  }
+
+  return $res;
 }
 
 sub count_error {
+  my $self = shift;
+  # 引数はリファレンス
+  # ブレースを使ってデリファレンス
+  my @logs = @{$self->{logs}};
+  my $err_total = 0;
+
+  for my $log (@logs) {
+    $_ = $log->{status};
+    if(m/^5\d+\d+$/) {
+      $err_total += 1;
+    }
+  }
+
+  return $err_total;
 }
 
 1;
